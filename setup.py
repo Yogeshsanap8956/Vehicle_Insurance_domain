c87a914900a1f287ea95034c738d02c8dbc639a1
Yogesh Vishnu Sanap <ysanap@novatechset.com>
2026-08-04 16:01:25 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author = "Yogesh",
+    author_email = "yogeshsanap77777@gmail.com",
+    packages = find_packages()
+)
